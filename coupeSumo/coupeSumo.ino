68b1ee911711dc3cc603606bc9ab152bc39233df
AlexandreMELIN <alexandre_melin@hotmail.fr>
2019-09-28 17:26:27 +0200

    Oupsi, i forgot to enable the motors before the loop

--- coupeSumo/coupeSumo.ino
+++ coupeSumo/coupeSumo.ino
@@ -1,31 +1,20 @@
-#include <vl53l0x_tuning.h>
-#include <vl53l0x_interrupt_threshold_settings.h>
-#include <vl53l0x_i2c_platform.h>
+
 #include <Adafruit_VL53L0X.h>
-#include <vl53l0x_api_core.h>
-#include <vl53l0x_types.h>
-#include <vl53l0x_device.h>
-#include <vl53l0x_api_ranging.h>
-#include <vl53l0x_platform.h>
-#include <vl53l0x_def.h>
-#include <vl53l0x_api_calibration.h>
-#include <vl53l0x_platform_log.h>
-#include <vl53l0x_api.h>
-#include <vl53l0x_api_strings.h>
 
 //Pins for motor
-#define ENABLE_A 0
-#define MOTOR_A1 1
-#define MOTOR_A2 2
-#define ENABLE_B 3
-#define MOTOR_B1 4
-#define MOTOR_B2 5
+#define ENABLE_A 5
+#define MOTOR_A1 6
+#define MOTOR_A2 7
+#define ENABLE_B 8
+#define MOTOR_B1 9
+#define MOTOR_B2 10
 
 //definition of Infrared sensor
 
 Adafruit_VL53L0X infraredSensor = Adafruit_VL53L0X();
 #define LEFT 0
 #define RIGHT 1
+#define TIMER 250
 //pins for color sensor
 #define colorSensorS0 6
 #define colorSensorS1 7
@@ -58,7 +47,9 @@ void setup() {
   //Setting frequency scaling
   digitalWrite(colorSensorS0, HIGH);
   digitalWrite(colorSensorS1, LOW);
-
+  //Enabling motor
+  digitalWrite(ENABLE_A, HIGH);
+  digitalWrite(ENABLE_B, HIGH);
 }
 
 void loop() {
@@ -69,24 +60,18 @@ void loop() {
   digitalWrite(colorSensorS3, LOW);
   redFrequency = pulseIn(colorSensorOutput, LOW);
   if (redFrequency < OFF_TABLE) { // black detected
-    if (measure.RangeStatus != 4 && measure.RangeMillimeter < 10) { //we're close to the target
+    if (measure.RangeStatus != 4 && measure.RangeMilliMeter < 10) { //we're close to the target
       moveForward();
     } else {
-      int nextDirection = random(0, 2);//0 = LEFT, 1 = RIGHT
-      if (nextDirection == LEFT) {
-        moveLeft();
-        delay(250);
-      } else {
-        moveRight();
-        delay(250);
-      }
+      changeDirection();
       moveForward();
     }
 
   } else { // white detected
     moveBackward();
+    changeDirection();
   }
-  delay(250);
+  delay(TIMER);
 }
 
 void moveBackward() {
@@ -115,4 +100,14 @@ void turnRight() {
   digitalWrite(MOTOR_A2, LOW);
   digitalWrite(MOTOR_B1, LOW);
   digitalWrite(MOTOR_B2, LOW);
+}
+void changeDirection(){
+  int nextDirection = random(0, 2);//0 = LEFT, 1 = RIGHT
+  if (nextDirection == LEFT) {
+    turnLeft();
+    delay(250);
+  } else {
+    turnRight();
+    delay(250);
+  }
 }
